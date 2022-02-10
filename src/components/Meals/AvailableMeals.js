20f7a7947b55bdd61b5a2ae870ed4aafc3d59d9e
Prithu Singh <prithusingh0327@gmail.com>
2022-02-10 14:49:24 +0530

    Added AvailableMeals component

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+import classes from './AvailableMeals.module.css';
+
+const DUMMY_MEALS = [
+    {
+      id: 'm1',
+      name: 'New York Special Pizza',
+      description: 'New York Style Pizza as you know and love',
+      price: 9.99,
+    },
+    {
+      id: 'm2',
+      name: 'Schnitzel',
+      description: 'A german specialty!',
+      price: 16.5,
+    },
+    {
+      id: 'm3',
+      name: 'Barbecue Burger',
+      description: 'American, raw, meaty',
+      price: 7.99,
+    },
+    {
+      id: 'm4',
+      name: 'Mojito',
+      description: 'Tangy, sweet, refreshing',
+      price: 4.99,
+    },
+  ];
+
+const AvailableMeals = () => {
+    const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
+
+    return <section className={classes.meals}>
+        <ul>
+            {mealsList}
+        </ul>
+    </section>
+}
+
+export default AvailableMeals;
