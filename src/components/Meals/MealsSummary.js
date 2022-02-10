34f8dde5a5f92d9537ac1894def6785883ea96ef
Prithu Singh <prithusingh0327@gmail.com>
2022-02-10 14:06:44 +0530

    Added MealsSummary component

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+    return <section className={classes.summary}>
+        <h2>Hungry? You're at the right place</h2>
+        <p>
+            Just Relax at Home, we will take care.
+        </p>
+        <p>
+            Fast Delivery.
+            Easy to order by Web or App.
+            Wide coverage around the world.
+        </p>
+    </section>
+}
